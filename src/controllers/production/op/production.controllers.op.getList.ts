d16babc28aa8be7161dac30aa207f882896fb6b4
David Morales <analistametodos@marrocar.local>
2024-05-15 10:35:41 -0500

    integracion de paginacion en controlador que expone lista de OP's

--- src/controllers/production/op/production.controllers.op.getList.ts
+++ src/controllers/production/op/production.controllers.op.getList.ts
@@ -2,6 +2,9 @@ import { Request, Response }    from 'express';
 import sql                      from 'mssql';
 import { Conexion }             from '../../../db/conection';
 import { HttpErrorResponse }    from '../../../utilities/httpErrorResponse';
+import * as yup from 'yup';
+import { dbParameters }         from '../../../interfaces/db/dbInterface';
+
 
 interface ApiDataResponse extends ApiResponse {
     data:Array<any>
@@ -19,18 +22,56 @@ interface DbResponse {
 interface ApiResponse {
     apiCode: -1 | 0 | 1,
     apiMessage: string,
-    data?:any
+    data?:any,
+    dataLength?:number;
+    date?: string
 }
 
 export const getList : (req:Request,res:Response) => Promise<any> = async (req:Request,res:Response) => {
+    
+    const { page, pageSize } = req.query;
+
+    const opDetailSchema = yup.object().shape({
+        page:yup.number().max(50).min(1),
+        pageSize:yup.number().max(50).min(1),
+    });
+
+    const params : Array<dbParameters> = [
+        {
+            name:'offset',
+            type:sql.Int,
+            value:page && pageSize ? (parseInt(page.toString())-1)*parseInt(pageSize.toString()):0
+        },
+        {
+            name: 'cantidad',
+            type: sql.Int,
+            value: page && pageSize? pageSize :20
+        }
+        
+    ]
+
+    try {
+        
+        await opDetailSchema.validate(req.params)
+
+    } catch (error) {
+        const errors:any=error
+        const apiResponse: ApiResponse = {
+            apiCode:-1,
+            date:new Date().toDateString(),
+            apiMessage: errors.errors[0] 
+        }
+        return res.status(500).json(apiResponse);
+    }
 
     try {
         const db = new Conexion(); 
-        const response : DbResponse = await db.execute('sp_gestion_ml_db_produccion_solicitud_op_lista_completa');
+        const response : DbResponse = await db.execute('sp_gestion_ml_db_produccion_solicitud_op_lista_completa', params);
         
         if(response.statusCode === -1){
             const apiResponse: ApiResponse ={
                 apiCode:-1,
+                date:new Date().toDateString(),
                 apiMessage:response.message || 'No se obtuvo los elementos'
             }
             return res.status(500).json(apiResponse)
@@ -39,6 +80,7 @@ export const getList : (req:Request,res:Response) => Promise<any> = async (req:R
         if(response.statusCode===0){
             const apiResponse: ApiResponse ={
                 apiCode:0,
+                date:new Date().toDateString(),
                 apiMessage:response.message || 'No se obtuvo los elementos'
             }
             return res.status(404).json(apiResponse)
@@ -47,6 +89,8 @@ export const getList : (req:Request,res:Response) => Promise<any> = async (req:R
         const apiResponse: ApiDataResponse = {
             apiCode:1,
             apiMessage: 'Consulta exitosa',
+            dataLength:response.data?.length,
+            date:new Date().toDateString(),
             data:response.data
         }
         return res.status(200).json(apiResponse);
@@ -54,6 +98,7 @@ export const getList : (req:Request,res:Response) => Promise<any> = async (req:R
     } catch (error) {
         const apiResponse: ApiResponse = {
             apiCode:-1,
+            date:new Date().toDateString(),
             apiMessage:'Error interno en el servidor'
         }
         return res.status(500).json(apiResponse)
