aed21c0bb50a48a1a1abcb9e340033a39ad2c037
David Morales <analistametodos@marrocar.local>
2024-07-22 15:43:09 -0500

    Integracion de controlador para solicitud de horarios de produccion

--- src/routes/sesion/sesion.api.ml.mobile.routes.ts
+++ src/routes/sesion/sesion.api.ml.mobile.routes.ts
@@ -2,6 +2,7 @@ import { Router }           from "express";
 import { getOperations }    from '../../controllers/sesion/mobile/sesion.controllers.mobile.getOperations';
 import { getMenu}           from '../../controllers/sesion/mobile/sesion.controllers.mobile.getListMenu';
 import { getAnomalyList }   from '../../controllers/sesion/mobile/sesion.controllers.mobile.getAnomalyList';
+import { getProductionScheduleList} from '../../controllers/sesion/mobile/sesion.controllers.mobile.getProductionScheduleList';
 
 export class SesionMobileRouter{
     public mobileRouter = Router();
@@ -9,8 +10,9 @@ export class SesionMobileRouter{
         this.inizialicer();
     }
     inizialicer(){
-        this.mobileRouter.get('/anomaly/list',  getAnomalyList);
-        this.mobileRouter.get('/operation/:id', getOperations);
-        this.mobileRouter.get('/menu/',      getMenu);
+        this.mobileRouter.get('/anomaly/list',              getAnomalyList);
+        this.mobileRouter.get('/production-schedule/list',  getProductionScheduleList);
+        this.mobileRouter.get('/operation/:id',             getOperations);
+        this.mobileRouter.get('/menu/',                     getMenu);
     }
 }
